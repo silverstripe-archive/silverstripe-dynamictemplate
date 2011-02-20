5cd4ac216c42b9c18b1a5306129447ab095a4e3f
Mark Stephens <mr.morphic@gmail.com>
2011-02-20 14:27:11 +1300

    MINOR: Moved files field to own file

---
+++ code/admin/DynamicTemplateFilesField.php
@@ -0,0 +1,163 @@
+<?php
+
+/**
+ * Form field that shows a list of files within a dynamic template. This
+ * is based on what's in the manifest, merged with files in the dynamic template.
+ * The following are possible:
+ * - a File object exists and it's in the manifest.
+ * - a relative link is in the manifest, pointing to a file outside
+ *   of the dynamic template, and no File exists.
+ * - a File object exists, but is not in the manifest, such as for images.
+ * 
+ * The tree display is done by treeTable.
+ */
+class DynamicTemplateFilesField extends FormField {
+	function __construct($name, $title = null, $value = null) {
+		parent::__construct($name, $title, $value, null);
+	}
+
+	/**
+	 * Generate a nested. Top level is the subfolders of the dynamic
+	 * template. Level beneath are the files. Each file is map,
+	 * with 'path' property, and 'ID' if it has an actual File object.
+	 * @todo Currently this totally ignores actions, effectively
+	 * only including what's in 'index' action.
+	 */
+	protected function calcTree() {
+		$null = null; // used with a reference. Don't remove.
+		$result = array();
+		$dt = $this->Value();
+		$manifest = $dt->getManifest();
+
+		$treeId = 1;
+
+		// Process the manifest
+		foreach ($manifest["index"] as $folder => $value) {
+			$subFolder = array("path" => $folder, "tree_id" => $treeId++, "children" => array());
+
+			if ($folder == "templates") {
+				foreach ($value as $type => $path) {
+					$item = array("path" => $path, "tree_id" => $treeId++);
+					$item["template_type"] = $type;
+					$subFolder["children"][] = $item;
+				}
+			}
+			else {
+				foreach ($value as $path) {
+					$item = array("path" => $path, "tree_id" => $treeId++);
+					$subFolder["children"][] = $item;
+				}
+			}
+			$result[] = $subFolder;
+		}
+
+		// Now process the file system.
+		if ($subFolders = $dt->AllChildren()) {
+			foreach ($subFolders as $subFolder) {
+				// locate the subfolder if it exists and return a reference to it.
+				$ref = &$null;
+				foreach ($result as $i => $sf) {
+					if ($sf['path'] == $subFolder->Name) $ref = &$result[$i];
+				}
+				if (!$ref) {
+					$newItem = array("path" => $subFolder->Name, "tree_id" => $treeId++, "children" => array());
+					$i = count($result);
+					$result[$i] = $newItem;
+					$ref = &$result[$i];
+				}
+
+				$files = $subFolder->AllChildren();
+				if ($files) foreach ($files as $file) {
+					// see if its in the array already.
+					$found = null;
+					foreach ($ref["children"] as $key => $item) {
+						if ($item['path'] == $file->Name) $found = $key;
+					}
+					if ($found === null) {
+						$item = array("path" => $file->Name, "ID" => $file->ID, "tree_id" => $treeId++);
+						$ref["children"][] = $item;
+					}
+					else
+						$ref["children"][$found]["ID"] = $file->ID;
+				}
+			}
+		}
+
+		return $result;
+	}
+
+	// Generate all markup for the tree.
+	function Field() {
+		$markup = "<table id=\"files-tree\">";
+
+		$tree = $this->calcTree();
+
+		foreach ($tree as $subFolder) {
+			$markup .= "<tr id=\"filetree-node-{$subFolder["tree_id"]}\">";
+			$markup .= "<td>{$subFolder["path"]}</td>";
+			$markup .= "</tr>";
+
+			foreach ($subFolder["children"] as $file) {
+				$markup .= "<tr id=\"filetree-node-{$file['tree_id']}\" class=\"child-of-filetree-node-{$subFolder['tree_id']}\">";
+				$markup .= "<td>{$file['path']}</td>";
+				$markup .= $this->actionsForFile($subFolder, $file);
+				$markup .= "</tr>";
+			}
+		}
+
+		$markup .= "</table>";
+		return $markup;
+	}
+
+	protected function actionsForFile($subFolder, $file) {
+		$markup = "";
+		if (!preg_match("/^.*\.([^.]+)$/", $file['path'], $matches)) return "";
+		if (!isset($matches[1])) return "";
+		$ext = $matches[1];
+
+		$hasProperties = true;
+		$hasEdit = in_array($ext, array("ss", "css", "js"));
+		$hasDelete = true;
+		$hasUnlink = false;
+
+		if (strpos($file['path'], "/") !== FALSE) {
+			$hasUnlink = true;
+			$hasDelete = false;
+			$hasEdit = false; // can't edit linked files
+		}
+
+		$markup .= "<td>";
+		if ($subFolder['path'] == "templates") {
+			// this is a template, so generate a combo for main and Layout
+			$items = array("" => "none", "main" => "main", "Layout" => "layout");
+			$markup .= "<select>";
+			foreach ($items as $key => $item) {
+				$markup .= '<option value="' . $key . '" ';
+				if (isset($file['template_type']) && $file['template_type'] == $key) $markup .= "selected";
+				$markup .= '>' . $item;
+				$markup .= '</option>';
+			}
+			$markup .= "</select>";
+		}
+		$markup .= "</td>";
+		$markup .= "<td>" . ($hasProperties ? '<button class="action-properties">Properties</button>' : '') . "</td>";
+		$markup .= "<td>" . ($hasEdit ? '<a href="' . $this->editLink($file) . '"><button class="action-edit">Edit source</button></a>' : '') . "</td>";
+		$markup .= "<td>";
+		if ($hasUnlink) $markup .= '<button class="action-unlink">Unlink</button>';
+		if ($hasDelete) $markup .= '<a href="' . $this->deleteLink($file) . '"><button class="action-delete">Delete</button></a>';
+		$markup .= "</td>";
+
+		return $markup;
+	}
+
+	// Given the file map from the tree, generate a link to the edit form. This is
+	// a link on DynamicTemplateAdmin that returns an edit form via ajax that
+	// will edit the content of the file.
+	function editLink($file) {
+		return "admin/dynamictemplates/LoadFileEditForm/{$file['ID']}";
+	}
+
+	function deleteLink($file) {
+		return "admin/dynamictemplates/DeleteFileFromTemplate/{$file['ID']}";
+	}
+}
