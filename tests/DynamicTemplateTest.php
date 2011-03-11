bc083ece4ef8b523f160c829a89d2a393e70bee9
Mark Stephens <mark@silverstripe.com>
2011-03-11 23:40:44 +1300

    MINOR: Updated the old unit tests for the new manifest structure, and added a bunch more unit tests around manifest.

--- tests/DynamicTemplateTest.php
+++ tests/DynamicTemplateTest.php
@@ -128,23 +128,22 @@ class DynamicTemplateTest extends SapphireTest {
 
 		$manifest = $folder->getManifest();
 
-		$this->assertTrue(isset($manifest['index']), "Default action derived with no manifest");
-		$this->assertEquals(count($manifest), 2, "only one action identified where no manifest (and metadata makes 2)");
-		$this->assertTrue(isset($manifest['index']['templates']), "templates found where no manifest");
-		$this->assertEquals(count($manifest['index']['templates']), 1, "one template found when no manifest");
-		$this->assertTrue(isset($manifest['index']['templates']['main']), "main template derived from no manifest");
+		$this->assertTrue(isset($manifest->actions['index']), "Default action derived with no manifest");
+		$this->assertEquals(count($manifest->actions), 1, "only one action identified where no manifest");
+		$this->assertTrue(isset($manifest->actions['index']['templates']), "templates found where no manifest");
+		$this->assertEquals(count($manifest->actions['index']['templates']), 1, "one template found when no manifest");
+		$this->assertTrue(isset($manifest->actions['index']['templates'][0]["type"]), "main template derived from no manifest");
 		$this->assertTrue(
 			strpos(
-				$manifest['index']['templates']["main"],
-				"assets/dynamic_templates/tmp__TemplateNoManifest/templates/test.ss") !== FALSE,
+				$manifest->actions['index']['templates'][0]['path'], 'test.ss') !== FALSE,
 			"picked up the right template without manifest"
 		);
 
-		$this->assertTrue(isset($manifest['index']['css']), "css present");
-		$this->assertEquals(count($manifest['index']['css']), 1, "exactly one css file");
+		$this->assertTrue(isset($manifest->actions['index']['css']), 'css present');
+		$this->assertEquals(count($manifest->actions['index']['css']), 1, 'exactly one css file');
 
-		$this->assertTrue(isset($manifest['index']['javascript']), "javascript present");
-		$this->assertEquals(count($manifest['index']['javascript']), 1, "exactly one javascript file");
+		$this->assertTrue(isset($manifest->actions['index']['javascript']), "javascript present");
+		$this->assertEquals(count($manifest->actions['index']['javascript']), 1, "exactly one javascript file");
 	}
 
 	/**
@@ -156,21 +155,19 @@ class DynamicTemplateTest extends SapphireTest {
 		$folder = DataObject::get_one("DynamicTemplate", "\"Name\"='tmp__TemplateWithManifest'");
 		$manifest = $folder->getManifest();
 
-		$this->assertTrue(isset($manifest['index']), "manifest has index action");
-		$this->assertEquals(count($manifest), 2, "manifest has one action (index and metadata)");
-		$this->assertTrue(isset($manifest['index']['templates']), "manifest default action has templates");
-		$this->assertEquals(count($manifest['index']['templates']), 1, "manifest default action has one template");
+		$this->assertTrue(isset($manifest->actions['index']), "manifest has index action");
+		$this->assertEquals(count($manifest->actions), 1, "manifest has one action");
+		$this->assertTrue(isset($manifest->actions['index']['templates']), "manifest default action has templates");
+		$this->assertEquals(count($manifest->actions['index']['templates']), 1, "manifest default action has one template");
 		$this->assertTrue(
-			strpos(
-				$manifest['index']['templates']["main"],
-				"dynamic_templates/tmp__TemplateWithManifest/templates/test.ss") !== FALSE,
+			strpos($manifest->actions['index']['templates'][0]['path'], 'test.ss') !== FALSE,
 			"manifest default action has test.ss"
 		);
 
 		// Test manifest is cached.
-		$ar = unserialize($folder->ManifestCache);
-		$this->assertTrue(is_array($ar), "Cached manifest is an array");
-		$this->assertTrue(isset($ar["index"]), "Cached manifest has index");
+		$m = unserialize($folder->ManifestCache);
+		$this->assertTrue(is_object($m) && is_a($m, 'DynamicTemplateManifest'), "Cached manifest is a DynamicTemplateManifest");
+		$this->assertTrue(isset($m->actions["index"]), "Cached manifest has index");
 	}
 
 	/**
@@ -191,7 +188,7 @@ class DynamicTemplateTest extends SapphireTest {
 		$this->assertTrue(preg_match("/.*\<script.*src=.*assets\/dynamic_templates\/tmp__TemplateWithManifest\/javascript\/test.js.*$/mU", $html) > 0, "javascript injected");
 	}
 
-	function testUnzipWithDir() {
+/*	function testUnzipWithDir() {
 		if (!class_exists("ZipArchive")) return;
 
 		$errors = array();
@@ -204,6 +201,67 @@ class DynamicTemplateTest extends SapphireTest {
 		// @todo Check that scandir returns the same 4 things
 		// @todo remove both artifacts created by the unit test, ensuring the file system is also synced.
 	}
+*/
+	function testManifestCreation() {
+		$manifest = new DynamicTemplateManifest();
 
+		// check initial state of the manifest
+		$this->assertTrue($manifest != null, "Manifest exists");
+		$this->assertTrue(
+			is_object($manifest) && is_a($manifest, 'DynamicTemplateManifest'),
+			'Manifest is a DynamicTemplateManifest object'
+		);
+		$this->assertTrue(isset($manifest->actions), 'Empty manifest has actions');
+		$this->assertEquals(count($manifest->actions), 0, 'Empty manifest has no actions');
+		$this->assertTrue(isset($manifest->metadata), 'Empty manifest has metadata');
+		$this->assertEquals(count($manifest->metadata), 0, 'Empty manifest has 0 metadata items');
+		$this->assertTrue(!$manifest->hasPath("foo.ss", "index"), "Doesn't have a path and we don't expect it to");
+
+		// add a template, non-standard action, not linked
+		$manifest->addPath('myaction', 'test.ss', 'main');
+		$this->assertTrue(isset($manifest->actions['myaction']), 'added action is present');
+		$this->assertTrue(!isset($manifest->actions['index']), "index action is not present, and shouldn't be");
+		$this->assertEquals(count($manifest->actions), 1, "There is 1 action");
+		$this->assertTrue(isset($manifest->actions['myaction']['templates']), 'Templates area exists in manifest');
+		$this->assertEquals(count($manifest->actions['myaction']['templates']), 1, 'There is 1 template in the manifest');
+		$this->assertEquals($manifest->actions['myaction']['templates'][0]['path'], 'test.ss', 'template we added is present');
+		$this->assertTrue(isset($manifest->actions['myaction']['templates'][0]['type']), 'template has type');
+		$this->assertEquals($manifest->actions['myaction']['templates'][0]['type'], 'main', 'template we added is flagged main');
+		$this->assertEquals($manifest->actions['myaction']['templates'][0]['linked'], false, 'template we added is correctly not flagged as linked');
+
+		$this->assertTrue($manifest->hasPath("test.ss", "myaction"), "Has a path and we do expect");
+		$this->assertTrue(!$manifest->hasPath("test.ss", "index"), "Doesn't have a path in index action and we don't expect it to");
+		$this->assertTrue(!$manifest->hasPath("foo.ss", "myaction"), "Doesn't have a path and we don't expect it to");
+
+		// add a linked template so we can test that
+		$manifest->addPath('myaction', '/somedir/foo.ss', 'Layout');
+		$this->assertEquals(count($manifest->actions['myaction']['templates']), 2, 'there are now two templates');
+		$this->assertEquals($manifest->actions['myaction']['templates'][0]['path'], 'test.ss', 'first template is still present');
+		$this->assertEquals($manifest->actions['myaction']['templates'][1]['path'], '/somedir/foo.ss', 'new template has correct path');
+		$this->assertTrue(isset($manifest->actions['myaction']['templates'][1]['type']), 'new template has type');
+		$this->assertEquals($manifest->actions['myaction']['templates'][1]['type'], 'Layout', 'new template we added is flagged Layout');
+		$this->assertEquals($manifest->actions['myaction']['templates'][1]['linked'], true, 'new template we added is correctly flagged as linked');
+		$this->assertEquals($manifest->actions['myaction']['templates'][0]['type'], 'main', 'original template still flagged main');
+
+		// add the original path again. it should not be added again
+		$manifest->addPath('myaction', 'test.ss', 'main');
+		$this->assertEquals(count($manifest->actions['myaction']['templates']), 2, 'there are still two templates');
+
+		// remove the original template
+		$manifest->removePath('myaction', 'test.ss');
+
+		$this->assertEquals(count($manifest->actions['myaction']['templates']), 1, 'There is only 1 remaining template in the manifest');
+		$this->assertEquals($manifest->actions['myaction']['templates'][1]['path'], '/somedir/foo.ss', 'remaining template has correct path');
+		$this->assertEquals($manifest->actions['myaction']['templates'][1]['type'], 'Layout', 'remaining template we added is still flagged Layout');
+	}
+
+	function testManifestStorage() {
+
+	}
+
+	function testManifestHelpers() {
+		$this->assertEquals(DynamicTemplateManifest::get_extension('file.ss'), '.ss', 'get extension successfully strips an extension');
+		$this->assertEquals(DynamicTemplateManifest::get_extension('path.with.dot/file.tar.gz'), '.gz', 'get extension on path with lots of dots');
+	}
 }
 
